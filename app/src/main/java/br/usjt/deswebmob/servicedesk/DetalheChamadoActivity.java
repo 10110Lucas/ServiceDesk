efe68b01ccd28ceb483d74bdc8b11e1d2c0aa333
Lucas de Souza Silva <lucas_900azul@hotmail.com>
2019-03-19 00:37:14 -0300

    Aula03 - Projeto Finalizado!

---
+++ app/src/main/java/br/usjt/deswebmob/servicedesk/DetalheChamadoActivity.java
@@ -0,0 +1,18 @@
+package br.usjt.deswebmob.servicedesk;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DetalheChamadoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalhe_chamado);
+        TextView nome = (TextView)findViewById(R.id.chamado_selecionado);
+        Intent intent = getIntent();
+        nome.setText(intent.getStringExtra(ListarChamadosActivity.DESCRICAO));
+    }
+}
